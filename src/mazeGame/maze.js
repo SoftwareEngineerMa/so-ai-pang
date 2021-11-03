a0ca8f095acf29455a6e1f4521977bd73ec26ed0
lizhuang <lizhuang@360.cn>
2021-11-03 11:38:15 +0800

    merge

--- src/mazeGame/maze.js
+++ src/mazeGame/maze.js
@@ -8,10 +8,6 @@ import { from } from 'rxjs';
 import setupCamera from '../utils/setCamera';
 import detectFace from '../utils/facedetect';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 
 
 import { ipcRenderer } from "electron";
@@ -100,10 +96,6 @@ class Maze{
     
     hxpSleep = false;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     awardNum = 0;
 
     constructor() {
